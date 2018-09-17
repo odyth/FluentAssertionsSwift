7412d32b7dcc01fd7688cf7b610c8345a3af74d5
odyth <jstofle@gmail.com>
2018-09-17 12:51:50 -0700

    added support for shouldBeApproximately and shouldNotBeApproximately

---
+++ FluentAssertions/FluentAssertions/FluentFloatExtensions.swift
@@ -0,0 +1,24 @@
+//
+//  FluentFloatExtensions.swift
+//  FluentAssertions
+//
+//  Created by odyth on 9/17/18.
+//  Copyright © 2018 Justin Stofle. All rights reserved.
+//
+
+import XCTest
+
+public extension Float
+{
+    public func shouldBeApproximately(_ expectedValue: Float, precision: Float, file: StaticString = #file, line: UInt = #line)
+    {
+        let num = abs(expectedValue - self)
+        XCTAssert(num <= precision, "Expected \(self) to approximate \(expectedValue) +/- \(precision), but \(self) differed by \(num)", file:file, line:line)
+    }
+    
+    public func shouldNotBeApproximately(_ unexpectedValue: Float, precision: Float, file: StaticString = #file, line: UInt = #line)
+    {
+        let num = abs(unexpectedValue - self)
+        XCTAssert(num > precision, "Expected \(self) to not approximate \(unexpectedValue) +/- \(precision), but \(self) only differed by \(num)", file:file, line:line)
+    }
+}
